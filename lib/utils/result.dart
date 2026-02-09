4cbf94384a0c3d5a31592f4a6b94236830b14be6
arturdsslva <arturdsslva@gmail.com>
2026-02-08 21:52:12 -0300

    add result and command patterns

---
+++ lib/utils/result.dart
@@ -0,0 +1,29 @@
+sealed class Result<T> {
+  const Result();
+
+  factory Result.ok(T value) => Ok(value);
+  factory Result.error(Exception error) => Error(error);
+}
+
+final class Ok<T> extends Result<T> {
+  const Ok(this.value);
+
+  final T value;
+
+  @override
+  String toString() => 'Result<$T>.ok($value)';
+}
+
+final class Error<T> extends Result<T> {
+  const Error(this.error);
+
+  final Exception error;
+
+  @override
+  String toString() => 'Result<$T>.error($error)';
+}
+
+extension ResultCast<T> on Result<T> {
+  Ok<T> get asOk => this as Ok<T>;
+  Error get asError => this as Error<T>;
+}
