0abad76b59ccf528007f9afc69aa3d17372f930a
arturdsslva <arturdsslva@gmail.com>
2026-02-08 23:23:37 -0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pulga_punch_clock/config/app/app_config.dart';
 import 'package:pulga_punch_clock/config/app/app_config.factory.dart';
+import 'package:pulga_punch_clock/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final appConfig = AppConfigFactory.fromDefines();
 
